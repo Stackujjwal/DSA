fed3a703d0deef2768b0ce505fdc2505c7ae83e6
Ujjwal Yadav <stackujjwal@gmail.com>
2026-07-26 23:49:05 +0530

    Time: 1 ms (16.11%), Space: 44.3 MB (88.1%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,16 +1,14 @@
 class Solution {
-    public int maximumWealth(int[][] arr) {
-        int maxSum=0;
-        for(int i =0;i<arr.length;i++){
-            int currVal=0;
-            for(int j=0;j<arr[i].length;j++){
-                currVal+=arr[i][j];
-            } 
-            if(currVal>maxSum){
-                maxSum=currVal;
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int i =0;i<accounts.length;i++){
+            int currval=0;
+            for(int j =0;j<accounts[i].length;j++){
+                currval+=accounts[i][j];
             }
-
+            max=Math.max(max,currval);
         }
-        return maxSum;
+        return max;
+
     }
 }
