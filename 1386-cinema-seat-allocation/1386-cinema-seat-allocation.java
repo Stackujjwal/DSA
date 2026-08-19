3d0e81011fa8a6d01596143b5d335dd1b1eec7d9
Ujjwal Yadav <stackujjwal@gmail.com>
2026-08-19 19:03:53 +0530

    Time: 19 ms (71.05%), Space: 51 MB (83.16%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -11,15 +11,12 @@ class Solution {
 
     for (final int seats : rowToSeats.values())
       if ((seats & 0b0111111110) == 0)
-        // Can fit 2 four-person groups.
-        ans += 2;
-      else if ((seats & 0b0111100000) == 0 || // The left is not occupied.
-               (seats & 0b0001111000) == 0 || // The middle is not occupied.
-               (seats & 0b0000011110) == 0)   // The right is notoccupied.
-        // Can fit 1 four-person group.
-        ans += 1;
 
-    // Any empty row can fit 2 four-person groups.
+        ans += 2;
+      else if ((seats & 0b0111100000) == 0 || 
+               (seats & 0b0001111000) == 0 || 
+               (seats & 0b0000011110) == 0)  
+        ans += 1;
     return ans + (n - rowToSeats.size()) * 2;
   }
 }
