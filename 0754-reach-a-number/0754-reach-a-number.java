bb12ec7eb3d8a4da619ec9f2ae64fd962b3ad426
Ujjwal Yadav <stackujjwal@gmail.com>
2026-09-09 11:51:19 +0530

    Time: 2 ms (69.62%), Space: 42.2 MB (41.59%) - LeetHub

---
+++ 0754-reach-a-number/0754-reach-a-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reachNumber(int target) {
+        target=Math.abs(target);
+        int steps=0;
+        int sum=0;
+        while(sum<target){
+            steps++;
+            sum+=steps;
+        }
+        while((sum-target)%2!=0){
+            steps++;
+            sum+=steps;
+        }
+        return steps;
+    }
+}
